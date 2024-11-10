d1f7a4cf5b86a5f03f52c6879575a7ed18a9e54c
CHAMUD12 <chamudgamage@gmail.com>
2024-11-10 17:52:37 +0530

    implement crop save

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/CropServiceIMPL.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/CropServiceIMPL.java
@@ -1,8 +1,14 @@
 package org.example.cropmonitoringsystembackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.cropmonitoringsystembackend.dao.CropDAO;
+import org.example.cropmonitoringsystembackend.dao.FieldDAO;
 import org.example.cropmonitoringsystembackend.dto.impl.CropDTO;
+import org.example.cropmonitoringsystembackend.entity.impl.Crop;
+import org.example.cropmonitoringsystembackend.entity.impl.Field;
+import org.example.cropmonitoringsystembackend.exception.DataPersistException;
 import org.example.cropmonitoringsystembackend.service.CropService;
+import org.example.cropmonitoringsystembackend.util.Mapping;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +18,23 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class CropServiceIMPL implements CropService {
+    private final CropDAO cropDAO;
+    private final FieldDAO fieldDAO;
+    private final Mapping mapping;
     @Override
     public void saveCrop(CropDTO cropDTO) {
-
+        Field field = fieldDAO.findById(cropDTO.getFieldCode())
+                .orElseThrow(() -> new DataPersistException("Invalid field code"));
+        Crop crop = mapping.convertToCrop(cropDTO);
+        crop.setField(field);
+        Crop savedCrop = cropDAO.save(crop);
+        try {
+            if (savedCrop == null) {
+                throw new DataPersistException("Can't save Crop");
+            }
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
