d89d36308d180d25ab425a30658e8992b953fa9e
CHAMUD12 <chamudgamage@gmail.com>
2024-11-11 13:10:58 +0530

    vehicle search using vehicle code or license plate number

--- src/main/java/org/example/cropmonitoringsystembackend/dao/VehicleDAO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dao/VehicleDAO.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface VehicleDAO extends JpaRepository<Vehicle, String> {
-    List<Vehicle> findByVehicleCodeOrVehicleCategory(String vehicleCode, String vehicleCategory);
+    List<Vehicle> findByVehicleCodeOrLicensePlateNumber(String vehicleCode, String licensePlateNumber);
 }
