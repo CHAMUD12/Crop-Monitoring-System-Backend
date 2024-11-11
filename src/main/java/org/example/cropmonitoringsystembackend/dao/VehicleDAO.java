5ce021e7d8a1efa49d19b05509181e6417a64f52
CHAMUD12 <chamudgamage@gmail.com>
2024-11-11 12:24:31 +0530

    crop search using crop code or common name

--- src/main/java/org/example/cropmonitoringsystembackend/dao/VehicleDAO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dao/VehicleDAO.java
@@ -2,9 +2,10 @@ package org.example.cropmonitoringsystembackend.dao;
 
 import org.example.cropmonitoringsystembackend.entity.impl.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface VehicleDAO extends JpaRepository<Vehicle, String> {
     List<Vehicle> findByVehicleCodeOrVehicleCategory(String vehicleCode, String vehicleCategory);
 }
