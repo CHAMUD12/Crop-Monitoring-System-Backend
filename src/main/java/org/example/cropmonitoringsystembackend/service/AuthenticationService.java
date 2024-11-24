6aa91e9cbdab9da8a43555ad0d91bdcaf31b0b4d
CHAMUD12 <chamudgamage@gmail.com>
2024-11-24 19:48:26 +0530

    implement AuthenticationService

---
+++ src/main/java/org/example/cropmonitoringsystembackend/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package org.example.cropmonitoringsystembackend.service;
+
+import org.example.cropmonitoringsystembackend.auth.request.SignInRequest;
+import org.example.cropmonitoringsystembackend.auth.request.SignUpRequest;
+import org.example.cropmonitoringsystembackend.auth.response.JWTAuthResponse;
+
+public interface AuthenticationService {
+    JWTAuthResponse signIn(SignInRequest signInRequest);
+
+    JWTAuthResponse signUp(SignUpRequest signUpRequest);
+
+    JWTAuthResponse refreshToken(String accessToken);
+}
