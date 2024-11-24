2f50ae4704a21806420736ae528f2afa3ccb2fc6
CHAMUD12 <chamudgamage@gmail.com>
2024-11-24 19:51:22 +0530

    jwt authResponse for signUp

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/AuthenticationServiceImpl.java
@@ -42,4 +42,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         // Return both tokens
         return JWTAuthResponse.builder().token(accessToken).refreshToken(refreshToken).build();
     }
+
+    @Override
+    public JWTAuthResponse signUp(SignUpRequest signUpRequest) {
+        UserDTO userDTO = UserDTO.builder().email(signUpRequest.getEmail()).password(passwordEncoder.encode(signUpRequest.getPassword())).role(signUpRequest.getRole()).build();
+        User savedUser = securityDAO.save(mapper.map(userDTO, User.class));
+
+        // Generate tokens
+        String accessToken = jwtService.generateToken(savedUser);
+        String refreshToken = jwtService.refreshToken(savedUser);
+
+        // Return both tokens
+        return JWTAuthResponse.builder().token(accessToken).refreshToken(refreshToken).build();
+    }
 }
