e16269e6874a279ac21a508b21a23fd47b1012a7
CHAMUD12 <chamudgamage@gmail.com>
2024-11-24 19:51:53 +0530

    jwt authResponse for refreshToken

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/AuthenticationServiceImpl.java
@@ -55,4 +55,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         // Return both tokens
         return JWTAuthResponse.builder().token(accessToken).refreshToken(refreshToken).build();
     }
+
+    @Override
+    public JWTAuthResponse refreshToken(String accessToken) {
+        String userName = jwtService.extractUserName(accessToken);
+//        if (!jwtService.isTokenValid(accessToken)) {
+//            throw new IllegalArgumentException("Invalid refresh token");
+//        }
+
+        // Generate a new access token
+        User user = userDAO.findByEmail(userName).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        String newAccessToken = jwtService.generateToken(user);
+
+        // Return the new access token and the same refresh token
+        return JWTAuthResponse.builder().token(newAccessToken).refreshToken(accessToken).build();
+    }
 }
