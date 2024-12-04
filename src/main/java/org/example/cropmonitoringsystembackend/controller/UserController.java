3714cec988e9e79959f78097dbc9b6133d908139
CHAMUD12 <chamudgamage@gmail.com>
2024-12-05 00:04:13 +0530

    add logback for user

--- src/main/java/org/example/cropmonitoringsystembackend/controller/UserController.java
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/UserController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import org.example.cropmonitoringsystembackend.dto.impl.UserDTO;
 import org.example.cropmonitoringsystembackend.service.UserService;
 import org.example.cropmonitoringsystembackend.util.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 public class UserController {
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -22,30 +25,35 @@ public class UserController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     List<UserDTO> getAllUser(){
+        logger.info("Fetching all users");
         return userService.getAllUser();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     UserDTO saveUser(@Valid @RequestBody UserDTO userDTO){
+        logger.info("Saving user with email: {}", userDTO.getEmail());
         return userService.saveUser(userDTO);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     void updateUser(@Valid @RequestBody UserDTO userDTO){
+        logger.info("Updating user with email: {}", userDTO.getEmail());
         userService.updateUser(userDTO.getEmail(),userDTO);
     }
 
     @DeleteMapping(value = "/{email}",consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     void deleteUser(@PathVariable("email") String email){
+        logger.info("Deleting user with email: {}", email);
         userService.deleteUser(email);
     }
 
     @PatchMapping(value = "/{email}/{role}",consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     UserDTO getUser(@PathVariable("email") String email, @PathVariable("role") Role role){
+        logger.info("Fetching user details for email: {} with role: {}", email, role);
         return userService.getUserDetails(email ,role);
     }
 }
